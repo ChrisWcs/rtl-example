24d7561dd6c06a15354bca3002e6435b276eea5d
Christian Hansen <ChrisWcs@users.noreply.github.com>
2019-02-26 15:36:25 -0600

    Add Problem component

--- src/Problem.js
+++ src/Problem.js
@@ -1,25 +1,33 @@
 import React from 'react'
 
 export default ({ total }) => {
-    const [state, setState] = React.useState({
-        value1: 0,
-        value2: 0,
-        
-    })
+    const [input1, setInput1] = React.useState(0)
+    const [input2, setInput2] = React.useState(0)
+    const [status, setStatus] = React.useState(false)
 
-    const check = () => {
-        setState()
-    }
+    const update1 = e => setInput1(Number(e.target.value))
+    const update2 = e => setInput2(Number(e.target.value))
 
-    return (
+    const checkAnswer = () => setStatus(input1 + input2 === total)
+
+    return(
         <div>
-            <input type='number' data-testid='input_one'/>
-            +
-            <input type='number' data-testid='input_two'/>
-            =
-            { total }
-            <button data-testid='input_one' onClick={}></button>
-            <h2 data-testid="result"></h2>
+            <label>Number 1
+                <input type="Number" 
+                       value={input1} 
+                       placeholder='Place the first number here!'
+                       onChange={update1}/>
+            </label>
+            <h2>+</h2>
+            <label>Number 2
+                <input type="Number" 
+                       value={input2}
+                       onChange={update2}/>
+            </label>
+            <h2>=</h2>
+            <h2>total</h2>
+            <button data-testid="check" onClick={checkAnswer}>Check Answer</button>
+            <h2 data-testid="result">{status ? 'Good' : 'Wrong'}</h2>
         </div>
     )
 }
--- src/Problem.spec.js
+++ src/Problem.spec.js
@@ -6,17 +6,17 @@ afterEach(cleanup)
 
 it("has two inputs that I can change to see if it works", () => {
     // Arrange
-    const { getByTestId } = render(<Problem total={30}/>)
+    const { getByTestId, getByPlaceholderText, getByLabelText } = render(<Problem total={30}/>)
     const checkResult = getByTestId('check')
-    const input_one = getByTestId('input_one')
-    const input_two = getByTestId('input_two')
+    const input_one = getByPlaceholderText('Place the first number here!')
+    const input_two = getByLabelText('Number 2')
     const result = getByTestId('result')
 
     // Act
-    fireEvent.change(input_one, { target: { value: '10' } })
-    fireEvent.change(input_two, { target: { value: '15' } })
+    fireEvent.change(input_one, { target: { value: 10 } })
+    fireEvent.change(input_two, { target: { value: 20 } })
     fireEvent.click(checkResult)
 
-    // Asserting that it now is 1
+    // Asserting that it was correct
     expect(result.textContent).toBe('Good')
 })
